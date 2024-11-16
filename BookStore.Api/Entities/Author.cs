bafea684ea22cd3f4a8194c4ac6648a8dd75d8a5
XeroChan <maciej2013.mp@gmail.com>
2024-11-16 14:26:33 +0100

    input validation

--- BookStore.Api/Entities/Author.cs
+++ BookStore.Api/Entities/Author.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BookStore.Api.Entities;
 
 public class Author
 {
     public int Id { get; set; }
     public int CredentialId { get; set; }
+    [Required]
+    [StringLength(60)]
     public required string AuthorName { get; set; }
+    [Required]
+    [StringLength(60)]
     public required string AuthorSurname { get; set; }
     // Navigation properties
     public Credential? Credential { get; set; }
