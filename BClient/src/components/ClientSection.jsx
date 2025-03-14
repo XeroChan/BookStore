161e6b7136b0e08d66a6338a31adb01b5761cc10
Mac <maciej2013.mp@gmail.com>
2025-03-14 20:14:56 +0100

    Fixed rentals not appearing instantly after renting a book.

--- BClient/src/components/ClientSection.jsx
+++ BClient/src/components/ClientSection.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Stack, Pagination } from "@mui/material";
-import RentalDatePicker from "../components/RentalDatePicker";
 import RentalSnackbar from "../components/RentalSnackbar";
 import ClientRentals from "../components/ClientRentals";
+import { fetchRentals } from "../api/data";
 
 const ClientSection = ({
-  books,
   rentals,
+  setRentals,
   user,
   dueDate,
   handleDueDateChange,
@@ -17,15 +17,15 @@ const ClientSection = ({
   client,
   formatDate,
   authors,
-  selectedBookId, // Accept selectedBookId as a prop
+  selectedBookId,
 }) => {
   const [currentRPage, setCurrentRPage] = useState(1);
   const RentalsPerPage = 4;
 
-  const filteredRentals = rentals.filter((rental) => {
+  const filteredRentals = Array.isArray(rentals) ? rentals.filter((rental) => {
     const userClientId = user?.clientId ? parseInt(user.clientId, 10) : null;
     return rental.clientId === userClientId;
-  });
+  }) : [];
 
   const totalPages = Math.ceil(filteredRentals.length / RentalsPerPage);
   const startIndex = (currentRPage - 1) * RentalsPerPage;
@@ -35,16 +35,16 @@ const ClientSection = ({
     setCurrentRPage(newPage);
   };
 
+  useEffect(() => {
+    // Fetch rentals data when rentalSuccess changes
+    if (rentalSuccess) {
+      fetchRentals(setRentals);
+    }
+  }, [rentalSuccess, setRentals]);
+
   return (
     <div>
       <h2>Katalog</h2>
-      {selectedBookId && (
-        <RentalDatePicker
-          dueDate={dueDate}
-          handleDueDateChange={handleDueDateChange}
-          handleBookRental={handleBookRental}
-        />
-      )}
 
       <RentalSnackbar
         rentalSuccess={rentalSuccess}
