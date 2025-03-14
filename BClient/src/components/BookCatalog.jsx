ad9faa47db4831a3b20a86755b1a9ad89f5b4589
Mac <maciej2013.mp@gmail.com>
2025-03-14 20:09:21 +0100

    Made book catalog work with authors table. Additional filtering options are now added. Fixed authors not being found by whole name and surname.

--- BClient/src/components/BookCatalog.jsx
+++ BClient/src/components/BookCatalog.jsx
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
 import { Stack, Pagination } from "@mui/material";
 import BookCard from "../components/BookCard";
-import SearchBar from "../components/SearchBar";
+import BookFilter from "../components/BookFilter";
 
-const BookCatalog = ({ books }) => {
+const BookCatalog = ({ books, onRent, selectedBookId, authors, isAdmin, handleEditBook, onViewDetails }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [searchQuery, setSearchQuery] = useState("");
+  const [filterCriteria, setFilterCriteria] = useState("title");
   const BooksPerPage = 5;
 
-  const filteredBooks = books.filter((book) =>
-    book.title.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  const filteredBooks = books.filter((book) => {
+    const value = book[filterCriteria]?.toLowerCase() || "";
+    if (filterCriteria === "author") {
+      if (!authors) return false;
+      const author = authors.find((author) => author.id === book.authorId);
+      const authorName = `${author?.authorName?.toLowerCase() || ""} ${author?.authorSurname?.toLowerCase() || ""}`;
+      return authorName.includes(searchQuery.toLowerCase());
+    }
+    return value.includes(searchQuery.toLowerCase());
+  });
+
   const startIndex = (currentPage - 1) * BooksPerPage;
   const endIndex = startIndex + BooksPerPage;
   const booksToShow = filteredBooks.slice(startIndex, endIndex);
@@ -19,14 +28,14 @@ const BookCatalog = ({ books }) => {
     setCurrentPage(newPage);
   };
 
-  const handleSearch = (searchQuery) => {
-    setSearchQuery(searchQuery);
-    setCurrentPage(1);
-  };
-
   return (
     <div>
-      <SearchBar setSearchQuery={handleSearch} />
+      <BookFilter
+        filterCriteria={filterCriteria}
+        setFilterCriteria={setFilterCriteria}
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+      />
       <div
         style={{
           display: "grid",
@@ -34,9 +43,20 @@ const BookCatalog = ({ books }) => {
           gap: "10px",
         }}
       >
-        {booksToShow.map((book) => (
-          <BookCard key={book.id} book={book} />
-        ))}
+        {booksToShow.map((book) => {
+          const author = authors.find((author) => author.id === book.authorId);
+          return (
+            <BookCard
+              key={book.id}
+              book={book}
+              author={author}
+              onRent={isAdmin ? null : onRent} // Conditionally render rental functionality
+              onViewDetails={onViewDetails}
+              selectedBookId={selectedBookId}
+              isAdmin={isAdmin}
+            />
+          );
+        })}
       </div>
       <Stack spacing={2} style={{ marginTop: "5vh", textAlign: "center" }}>
         <Pagination
