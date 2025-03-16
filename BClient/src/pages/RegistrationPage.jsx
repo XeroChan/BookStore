f630e1ccef9e9de63dca4faa4824dc5aed03ca5e
Mac <maciej2013.mp@gmail.com>
2025-03-16 11:59:21 +0100

    added secure password policy with validation and on screen hints for each, changed form styling

--- BClient/src/pages/RegistrationPage.jsx
+++ BClient/src/pages/RegistrationPage.jsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
+  Box,
   Container,
   Typography,
   TextField,
   Button,
   Snackbar,
-  FormControlLabel,
-  Checkbox,
   Alert,
 } from "@mui/material";
 import { styled } from "@mui/system";
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import ErrorIcon from '@mui/icons-material/Error';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
 
 const RegistrationContainer = styled(Container)({
   display: "flex",
@@ -36,11 +40,18 @@ export const RegistrationPage = () => {
     telephone: "",
     username: "",
     password: "",
-    isAdmin: false,
   });
 
   const [registrationSuccess, setRegistrationSuccess] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [passwordValidations, setPasswordValidations] = useState({
+    length: false,
+    digits: false,
+    lowercase: false,
+    uppercase: false,
+    special: false,
+    noRepeats: false,
+  });
 
   const navigate = useNavigate();
 
@@ -48,12 +59,42 @@ export const RegistrationPage = () => {
     const { name, value, type, checked } = e.target;
     setFormData((prevData) => ({
       ...prevData,
-      [name]: type === "checkbox" ? checked : value,
+      [name]: type === 'checkbox' ? checked : value,
     }));
+
+    if (name === 'password') {
+      validatePasswordHints(value);
+    }
+  };
+
+  const validatePasswordHints = (password) => {
+    setPasswordValidations({
+      length: password.length >= 10 && password.length <= 19,
+      digits: (password.match(/\d/g) || []).length >= 2,
+      lowercase: (password.match(/[a-z]/g) || []).length >= 2,
+      uppercase: (password.match(/[A-Z]/g) || []).length >= 2,
+      special: (password.match(/[@#$%!]/g) || []).length >= 2,
+      noRepeats: !/(.)\1{1}/.test(password),
+    });
+  };
+  const [showPassword, setShowPassword] = useState(false);
+
+  const validatePassword = (password) => {
+    const passwordRegex = /^(?!.*(.)\1{1})(?=(.*[\d]){2,})(?=(.*[a-z]){2,})(?=(.*[A-Z]){2,})(?=(.*[@#$%!]){2,})(?:[\da-zA-Z@#$%!]){10,20}$/;
+    return passwordRegex.test(password);
+  };
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
   };
 
   const handleRegistration = async (e) => {
     e.preventDefault();
+
+    if (!validatePassword(formData.password)) {
+      setErrorMessage('Hasło nie jest zgodne z polityką bezpieczeństwa.');
+      return;
+    }
 
     const registrationData = {
       name: formData.name,
@@ -112,7 +153,17 @@ export const RegistrationPage = () => {
           value={formData.name}
           onChange={handleChange}
           required
-          inputProps={{ maxLength: 50 }}
+          inputProps={{ maxLength: 50}}
+          InputLabelProps={{
+            sx: {
+              color: '#9bc9db',
+            },
+          }}
+          InputProps={{
+            sx: {
+              color: '#ffffff',
+            },
+          }}
         />
         <TextField
           label="Nazwisko"
@@ -123,6 +174,16 @@ export const RegistrationPage = () => {
           onChange={handleChange}
           required
           inputProps={{ maxLength: 50 }}
+          InputLabelProps={{
+            sx: {
+              color: '#9bc9db',
+            },
+          }}
+          InputProps={{
+            sx: {
+              color: '#ffffff',
+            },
+          }}
         />
         <TextField
           label="Email"
@@ -133,6 +194,16 @@ export const RegistrationPage = () => {
           onChange={handleChange}
           required
           inputProps={{ maxLength: 100 }}
+          InputLabelProps={{
+            sx: {
+              color: '#9bc9db',
+            },
+          }}
+          InputProps={{
+            sx: {
+              color: '#ffffff',
+            },
+          }}
         />
         <TextField
           label="Telefon"
@@ -143,6 +214,16 @@ export const RegistrationPage = () => {
           onChange={handleChange}
           required
           inputProps={{ maxLength: 9, pattern: "[0-9]{9}" }}
+          InputLabelProps={{
+            sx: {
+              color: '#9bc9db',
+            },
+          }}
+          InputProps={{
+            sx: {
+              color: '#ffffff',
+            },
+          }}
         />
         <TextField
           label="Nazwa użytkownika"
@@ -153,28 +234,83 @@ export const RegistrationPage = () => {
           onChange={handleChange}
           required
           inputProps={{ maxLength: 20 }}
+          InputLabelProps={{
+            sx: {
+              color: '#9bc9db',
+            },
+          }}
+          InputProps={{
+            sx: {
+              color: '#ffffff',
+            },
+          }}
         />
         <TextField
           label="Hasło"
           variant="outlined"
-          type="password"
+          type={showPassword ? "text" : "password"}
           name="password"
           value={formData.password}
           onChange={handleChange}
           required
           inputProps={{ maxLength: 20 }}
+          InputLabelProps={{
+            sx: {
+              color: '#9bc9db',
+            },
+          }}
+          InputProps={{
+            sx: {
+              color: '#ffffff',
+            },
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
         />
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={formData.isAdmin}
-              onChange={handleChange}
-              name="isAdmin"
-            />
-          }
-          label="Admin"
-        />
-        <Button type="submit" variant="contained" color="primary">
+        <Box sx={{ marginTop: 2 }}>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Typography variant="body2">Hasło musi mieć od 10 do 20 znaków.</Typography>
+            {passwordValidations.length ? <CheckCircleIcon color="success" /> : <ErrorIcon color="error" />}
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Typography variant="body2">Hasło musi zawierać przynajmniej 2 cyfry.</Typography>
+            {passwordValidations.digits ? <CheckCircleIcon color="success" /> : <ErrorIcon color="error" />}
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Typography variant="body2">Hasło musi zawierać przynajmniej 2 małe litery.</Typography>
+            {passwordValidations.lowercase ? <CheckCircleIcon color="success" /> : <ErrorIcon color="error" />}
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Typography variant="body2">Hasło musi zawierać przynajmniej 2 duże litery.</Typography>
+            {passwordValidations.uppercase ? <CheckCircleIcon color="success" /> : <ErrorIcon color="error" />}
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Typography variant="body2">Hasło musi zawierać przynajmniej 2 z następujących znaków specjalnych (@#$%!).</Typography>
+            {passwordValidations.special ? <CheckCircleIcon color="success" /> : <ErrorIcon color="error" />}
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Typography variant="body2">Hasło nie może posiadać następujących po sobie powtarzających się znaków.</Typography>
+            {passwordValidations.noRepeats ? <CheckCircleIcon color="success" /> : <ErrorIcon color="error" />}
+          </Box>
+        </Box>
+        <Button type="submit" variant="contained" color="primary"
+        sx={{
+          textDecoration: 'inherit',
+          '&:hover': {
+            color: '#000000',
+            backgroundColor: '#89c7fa',
+          },
+        }}
+        >
           Zarejestruj się
         </Button>
       </RegistrationForm>
