e0501cd199ac18252995255ab3ef7cab9a48fef8
Mac <maciej2013.mp@gmail.com>
2025-03-16 11:51:57 +0100

    added search option for authors

--- BClient/src/components/AuthorsTable.jsx
+++ BClient/src/components/AuthorsTable.jsx
@@ -1,20 +1,47 @@
 import React, { useState } from 'react';
-import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper, Button, Stack, Pagination } from '@mui/material';
+import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper, Button, Stack, Pagination, TextField } from '@mui/material';
 import { getSubscriptionId, handleOpenDialog, handleSubscribeToAuthor } from '../helpers/storePageHelpers';
 
 const AuthorsTable = ({ user, isAdmin, authors, subscriptions, setNewPublications, setSubscriptions, setSelectedSubscription, setOpenDialog, credentialId }) => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState("");
   const authorsPerPage = 5;
+
   const handlePageChange = (_event, newPage) => {
     setCurrentPage(newPage);
   };
 
+  const filteredAuthors = authors.filter((author) =>
+    `${author.authorName.toLowerCase()} ${author.authorSurname.toLowerCase()}`.includes(searchQuery.toLowerCase())
+  );
+
   const startIndex = (currentPage - 1) * authorsPerPage;
   const endIndex = startIndex + authorsPerPage;
-  const authorsToShow = authors.slice(startIndex, endIndex);
+  const authorsToShow = filteredAuthors.slice(startIndex, endIndex);
 
   return (
     <div>
+      <TextField
+        label="Wyszukaj autora"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        InputLabelProps={{
+          sx: {
+            color: "#9bc9db",
+          },
+        }}
+        InputProps={{
+          sx: {
+            color: "#ffffff",
+            "&:hover .MuiOutlinedInput-notchedOutline": {
+              borderColor: "#89c7fa",
+            },
+          },
+        }}
+      />
       <TableContainer component={Paper}>
         <Table>
           <TableHead>
@@ -48,7 +75,7 @@ const AuthorsTable = ({ user, isAdmin, authors, subscriptions, setNewPublication
       </TableContainer>
       <Stack spacing={2} style={{ marginTop: '1rem', textAlign: 'center' }}>
         <Pagination
-          count={Math.ceil(authors.length / authorsPerPage)}
+          count={Math.ceil(filteredAuthors.length / authorsPerPage)}
           page={currentPage}
           onChange={handlePageChange}
           variant="outlined"
