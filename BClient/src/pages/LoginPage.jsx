3c335275e75d583fcaeefa437a11b03610065dde
Mac <maciej2013.mp@gmail.com>
2025-03-16 11:58:24 +0100

    moved from alerts to on page error indicators, added the option to show/hide password

--- BClient/src/pages/LoginPage.jsx
+++ BClient/src/pages/LoginPage.jsx
@@ -1,6 +1,10 @@
-import { Container, Typography, TextField, Button } from "@mui/material";
+import { Container, Typography, TextField, Button, Alert } from "@mui/material";
 import { styled } from "@mui/system";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
 
 const LoginContainer = styled(Container)({
   display: "flex",
@@ -19,7 +23,13 @@ const LoginForm = styled("form")({
 });
 
 export const LoginPage = ({ setIsAuthenticated }) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -28,31 +38,36 @@ export const LoginPage = ({ setIsAuthenticated }) => {
     const password = event.target.password.value;
 
     try {
-        const response = await fetch('http://localhost:5088/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                username: username,
-                password: password,
-            }),
-        });
+      const response = await fetch("http://localhost:5088/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      });
 
-        if (response.ok) {
-            const token = await response.text();
-            sessionStorage.setItem('authToken', token);
-            setIsAuthenticated(true);
-            navigate('/store');
-        } else {
-            const errorData = await response.json();
-            console.error('Login error:', errorData.message);
-            alert(errorData.message);
-        }
+      if (response.ok) {
+        const token = await response.text();
+        sessionStorage.setItem("authToken", token);
+        setIsAuthenticated(true);
+        navigate("/store");
+      } else {
+        const errorData = await response.json();
+        console.error("Login error:", errorData.message);
+        setErrorMessage(errorData.message);
+      }
     } catch (error) {
-        console.error('Login error:', error);
+      console.error("Login error:", error);
+      setErrorMessage("An unexpected error occurred. Please try again later.");
     }
-};
+  };
+
+  const handleAlertClose = () => {
+    setErrorMessage("");
+  };
 
   return (
     <LoginContainer>
@@ -66,18 +81,62 @@ export const LoginPage = ({ setIsAuthenticated }) => {
           type="text"
           required
           name="username"
+          InputLabelProps={{
+            sx: {
+              color: "#9bc9db",
+            },
+          }}
+          InputProps={{
+            sx: {
+              color: '#ffffff',
+            },
+          }}
         />
         <TextField
           label="Hasło"
           variant="outlined"
-          type="password"
+          type={showPassword ? "text" : "password"}
           required
           name="password"
+          InputLabelProps={{
+            sx: {
+              color: "#9bc9db",
+            },
+          }}
+          InputProps={{
+            sx: {
+              color: '#ffffff',
+            },
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
         />
-        <Button type="submit" variant="contained" color="primary">
+        <Button type="submit" variant="contained" color="primary"
+        sx={{
+          textDecoration: 'inherit',
+          '&:hover': {
+            color: '#000000',
+            backgroundColor: '#89c7fa',
+          },
+        }}
+        >
           Zaloguj się
         </Button>
       </LoginForm>
+      {errorMessage && (
+        <Alert severity="error" onClose={handleAlertClose}>
+          {errorMessage}
+        </Alert>
+      )}
     </LoginContainer>
   );
 };
